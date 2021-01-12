4abffbbdc2872c5ce08206eac1a97638a223af66
Christopher Kumm <ChrisLKumm@protonmail.com>
2021-01-12 20:18:35 +0300

    Added test to make sure long messages return a 400 response

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+
 from rest_framework.test import APITestCase
 from rest_framework import status
 
@@ -70,6 +72,19 @@ class SubmitInquiryAPITestCase(APITestCase):
                 # 'name': 'Chris Kumm',
                 'email': 'ck@aol',
                 'message': 'Hi!'
+            }
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_long_message_returns_400(self):
+        long_message = 'x' * (settings.MESSAGE_MAX_LENGTH + 1)
+        response = self.client.post(
+            '/api/submit-inquiry/',
+            {
+                'name': 'Chris Kumm',
+                'email': 'ck@aol.com',
+                'message': long_message
             }
         )
 
