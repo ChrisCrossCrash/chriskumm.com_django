979c92506df34360e616e8d0746cda9d551ec25e
Christopher Kumm <ChrisLKumm@protonmail.com>
2021-01-12 16:20:43 +0300

    Added `AllowAny` permissions decorator to `submit_inquiry` view

--- core/views.py
+++ core/views.py
@@ -1,11 +1,13 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 
 from .serializers import InquirySerializer
 
 
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def submit_inquiry(request):
     serializer = InquirySerializer(data=request.data)
     if serializer.is_valid():
