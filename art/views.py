08f9ee70b7e87cb197c4aaaf7b9e8be065968de6
Christopher Kumm <ChrisLKumm@protonmail.com>
2021-11-13 15:07:19 +0300

    new artist, location, and style list urls

--- art/views.py
+++ art/views.py
@@ -1,8 +1,13 @@
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.generics import ListAPIView
 
-from .models import Piece
-from .serializers import InstaArtSerializer
+from .models import Piece, Artist, Location, Style
+from .serializers import (
+    InstaArtSerializer,
+    ArtistSerializer,
+    LocationSerializer,
+    StyleSerializer,
+)
 
 
 class ArtPaginationClass(PageNumberPagination):
@@ -13,6 +18,21 @@ class PieceList(ListAPIView):
     queryset = Piece.objects.all()
     serializer_class = InstaArtSerializer
     pagination_class = ArtPaginationClass
+
+
+class ArtistList(ListAPIView):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
+
+
+class LocationList(ListAPIView):
+    queryset = Location.objects.all()
+    serializer_class = ArtistSerializer
+
+
+class StyleList(ListAPIView):
+    queryset = Style.objects.all()
+    serializer_class = StyleSerializer
 
 
 class PiecesByArtistList(ListAPIView):
