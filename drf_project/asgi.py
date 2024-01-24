9e7fc41732bf3106d1bf80ac74aac6684ae8f1c8
Christopher Kumm <chrislkumm@protonmail.com>
2024-01-23 21:57:10 -0600

    Install Channels

--- drf_project/asgi.py
+++ drf_project/asgi.py
@@ -5,12 +5,23 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+
+For Channels documentation, see:
+https://channels.readthedocs.io/en/latest/installation.html
 """
 
 import os
 
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drf_project.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
 
-application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+    }
+)
