3b0dca8a2dd95753ea4a19311e372794523cdc65
Christopher Kumm <chrislkumm@protonmail.com>
2024-01-24 17:27:03 -0600

    Commented out websocket routes in routing.py
    No consideration was made for security on these routes, so I'm commenting them out for now.

--- chat/routing.py
+++ chat/routing.py
@@ -3,7 +3,7 @@ from django.urls import re_path, path
 from . import consumers
 
 websocket_urlpatterns = [
-    path("ws/echo/", consumers.EchoConsumer.as_asgi()),
-    path("ws/ai-chat/", consumers.AIChatConsumer.as_asgi()),
-    re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
+    # path("ws/echo/", consumers.EchoConsumer.as_asgi()),
+    # path("ws/ai-chat/", consumers.AIChatConsumer.as_asgi()),
+    # re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
 ]
