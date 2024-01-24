970aaffdbad2148be591cbad9eae9292a37702e6
Christopher Kumm <chrislkumm@protonmail.com>
2024-01-23 21:22:57 -0600

    Implement basic creature creation

--- ai_pals/urls.py
+++ ai_pals/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import index
+from .views import new_character
 
 urlpatterns = [
-    path("", index, name="ai_pals"),
+    path("", new_character, name="ai_pals"),
 ]
